c464b367a47bf8c3cbddb77d629d095ca11ac95f
Calvin Bulla <calvin.bulla@gmail.com>
2016-08-25 13:10:16 +0200

    Minor modification of test

--- test/hello.c
+++ test/hello.c
@@ -1,14 +1,15 @@
-#include "../src/hello.h"
 #include "cafe.h"
+
+#include "../src/hello.h"
 
 Cafe {
     Describe("Hello module") {
         Describe("add_five()") {
             It("should work with positive numbers") {
-                Assert(add_five(14) == 19)
+                Assert(add_five(14) == 19);
             }
             It("should work with negative numbers") {
-                Assert(add_five(-3) == 2)
+                Assert(add_five(-3) == 2);
             }
             It("should be full of bugs") { Error("This line always failes") }
             It("should do magic") {}
